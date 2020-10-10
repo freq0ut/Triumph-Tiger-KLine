0814c4876009f79b87db7e2974ecfcc97fe1fb6a
Zack Goyetche <zack.goyetche@protonmail.com>
2020-10-10 00:08:16 -0700

    test commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // start serial port at 9600 bps:
+  Serial.begin(9600);
+  while (!Serial) {;} // wait for serial port to connect. Needed for native USB port only
+}
+
+void loop() 
+{
+  Serial.write("Hello world.");
+  delay(1000);
+}
